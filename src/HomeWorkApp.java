9a65af9f9ca2be8d66b2a6746d664d05d7af9669
AnickaDiver <ann-jessica@yandex.ru>
2021-08-08 15:04:58 +0300

    ДЗ выполнено.

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,47 +1,57 @@
+import java.util.Arrays;
+
 public class HomeWorkApp {
     public static void main(String[] args) {
 
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-    }
+        int [] task1 = new int[] {1, 0, 1, 1, 0, 1};
+        for (int u=0; u< task1.length; u++) {
+            if (task1[u] == 1) {
+                task1[u] = 0;
+            }
+            else if (task1[u] == 0) {
+                task1[u] = 1;
+            }
 
-    public static void printThreeWords() {
-        System.out.println("Orange\nBanana\nApple");
-    }
+        }
+        System.out.println(Arrays.toString(task1));
 
-    public static void checkSumSign() {
-        int a = 7;
-        int b = 21;
-        int c = a +b;
-        if (c >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
+int[] task2 = new int [100];
+for (int i=0; i<task2.length; i++) {
+   task2[i] = i+1;
+}
+System.out.println(Arrays.toString(task2));
+
+       int [] task3 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+       for (int num = 0; num < task3.length; num++) {
+           if (task3[num] < 6) {
+               task3[num] = task3[num]*2;
+           }
+       }
+       System.out.println(Arrays.toString(task3));
+
+        int [] [] task4 = new int [5] [5];
+        for (int a = 0; a < task4.length; a++) {
+            for (int b = 0; b < task4[a].length; b++) {
+                if (a == b) {
+                    task4 [a] [b] = 5;
+                }
+                System.out.print(task4[a][b]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        PrintMessage(81, 6);
+}
+
+    public static void PrintMessage (int initialValue, int len) {
+        int [] task5 = new int [len];
+        for (int i = 0; i < task5.length; i++) {
+            task5[i] = initialValue;
         }
 
+        System.out.println(Arrays.toString(task5));
+
     }
 
-    public static void printColor() {
-        int value = 5;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
-        }
-    }
 
-    public static void compareNumbers() {
-        int a = 8;
-        int b = 31;
-        if (a >= b) {
-            System.out.println("a>=b");
-        }
-        else {
-            System.out.println("a<b");
-        }
-    }
 }
